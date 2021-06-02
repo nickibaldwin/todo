4dbc637facec79ae085493813af3625771bb8c6b
Nicki Baldwin <76967823+nickibaldwin@users.noreply.github.com>
2021-06-01 22:51:14 -0700

    phase 1

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  <header>
+    <Nav bg="blue" expand="lg">
+      <Nav.Link href="#">Home</Nav.Link>
+    </Nav>
+  </header>
+}
+
+export default Header;
